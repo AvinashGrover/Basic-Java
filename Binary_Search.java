20773e3fd170835db88ebe896d4d043e0853e2ba
AvinashGrover <86574317+AvinashGrover@users.noreply.github.com>
2022-10-04 23:18:59 +0530

    Add files via upload

---
+++ Binary_Search.java
@@ -0,0 +1,28 @@
+package javaProgramming;
+
+public class Binary_Search {
+
+	public static int binarySearch(int[] input, int elem) {
+		int start = 0;
+		int end = input.length -1;
+		while(start <= end) {
+			int mid = (start + end) / 2;
+			if(elem == input[mid]) {
+				return mid;
+			}else if(elem > input[mid]) {
+				start = mid +1;
+			}else {
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		int[]input = {2,4,5,8,9,15,21,28};
+		int index = binarySearch(input,28);
+		System.out.println(index);
+		
+	}
+
+}

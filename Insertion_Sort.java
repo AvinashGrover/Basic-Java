97f33b3a57aa3021a9502c1b4619e1173c76be10
AvinashGrover <86574317+AvinashGrover@users.noreply.github.com>
2022-10-07 21:49:27 +0530

    Add files via upload

---
+++ Insertion_Sort.java
@@ -0,0 +1,29 @@
+package javaProgramming;
+
+public class Insertion_Sort {
+	
+	public static void insertionSort(int arr[]) {
+		
+		for(int i=1; i < arr.length; i++) {
+			
+			int j = i - 1;
+			int temp = arr[i];
+			while(j>= 0 && arr[j]> temp) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		int arr[] = {6,4,3,5,2};
+		insertionSort(arr);
+		for(int i = 0; i < arr.length; i++) {
+		System.out.print(arr[i]+" "); 
+		}
+
+	}
+
+}

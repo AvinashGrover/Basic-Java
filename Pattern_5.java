3e119f12f6a5158a60988e83d176b19629dce563
AvinashGrover <86574317+AvinashGrover@users.noreply.github.com>
2022-10-15 23:25:55 +0530

    Add files via upload

---
+++ Pattern_5.java
@@ -0,0 +1,51 @@
+package javaProgramming;
+
+public class Pattern_5 {
+
+	public static void main(String[] args) {
+		
+		int n =5;
+		// upper half
+		for(int i=1; i<=n; i++) {
+			// 1st part
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			
+			//spaces
+			int space = 2*(n-i);
+			for(int j=1; j<=space;j++) {
+				System.out.print(" ");
+			}
+			
+			//2nd part
+			for(int j=1;j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		 // lower half
+		for(int i=n; i>=1; i--) {
+			// 1st part
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			
+			//spaces
+			int space = 2*(n-i);
+			for(int j=1; j<=space;j++) {
+				System.out.print(" ");
+			}
+			
+			//2nd part
+			for(int j=1;j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+               
